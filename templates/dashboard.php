28a519e7c0c9b8d4acf6a6bd5a66590951b92a5c
JAY <j.ilukhor@gmail.com>
2015-02-02 13:04:16 +0100

    My first commit
    Just create the dashboard.php file along with the necessary CSS and SASS
    file

---
+++ templates/dashboard.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta id="meta_referrer" content="default" name="referrer" />
+
+		<title>Stvdi</title>
+
+		<link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+
+		<link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+
+		<!--[if IE]>
+			<link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+		<![endif]-->
+
+		<script src="">
+			Reload the page to get source for: url
+		</script>
+
+	</head>
+
+	<body>
+
+		<div id="pageContent>
+
+			<div id="topBar">
+
+				<h1>
+					<a href="">
+
+						<i>
+							<u>Stvdi</u>
+						</i>
+
+					</a>
+
+				</h1>
+
+			</div>
+			<!-- End #topBar -->
+
+
+		</div>
+		<!-- End #pageContent -->
+
+
+	</body>
+</html>
