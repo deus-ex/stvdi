b0900c9523f7c6154a87f4d261e3c9d06e1e2a28
TheChosen1 <evolutioneerbeyond@yahoo.com>
2015-03-18 16:31:13 +0100

    ::: created login.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-/*
-  // Include important f
+
+  // Include important files
   include( 'includes/config/config.inc.php' );
   include( 'includes/loader.inc.php' );
 
@@ -47,73 +47,5 @@
     )
   )
 );
-*/
+
 ?>
-
-<!DOCTYPE html>
-<html>
-	<head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width"/>
-		<title>Stvdi: Login</title>
-		<link href="stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
-		<link href="stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
-	</head>
-
-	<body>
-		
-        <div class="loginWrapper">
-        	<div class="loginContainer">
-            	<div class="loginContent text-Black Colour-White">
-                	<div class="bodyRow clearFix">
-                        <div class="divided">
-                            <img src="images/backgrounds/school-children-small1-1500x643.png" class="loginImage noMobilePhone">
-                            
-                            <div class="loginTable Colour-White borderAll transparency">
-                                <!-- This is where the content starts -->
-                                <div class="spanFull"><h3 class="strong">STVDI</h3></div>
-                                <div class="spanFull"><h4>Login to your Stvdi account</h4></div>
-                                <form id="record" method="post" action="template/dashboard.php">
-                                <div class="spanFull selectMenuWrapper underLine">
-                                    <input id="selectType" name="selectType" type="text" autocomplete="off" readonly placeholder="Select Login Type..." class="selectMenu Colour-Lgray">
-                                    <ul class="borderAll">
-                                        <li><a href="#"><h6>Student</h6></a></li>
-                                        <li><a href="#"><h6>Staff</h6></a></li>
-                                        <li><a href="#"><h6>Parent</h6></a></li>
-                                    </ul>
-                                </div>
-                                <div class="spanFull"><input id="user" name="user" type="text" autocomplete="off" placeholder="Email / Username" class="Colour-White"></div>
-                                <div class="spanFull"><input id="pass" name="pass" type="password" autocomplete="off"  placeholder="Password" class="Colour-White"></div>
-                                <div class="spanFull">
-                                    <input id="signedin" type="checkbox" name="signedin" value="1">
-                                    <label for="signedin"><span><span></span></span><h6 class="strong">Keep me signed in</h6></label>
-                                </div>
-                                <div class="spanFull"><button class="text-White Colour-Blue dropShadow">Sign In</button></div>
-                                <div class="spanFull loginLinks"><a href="#"><h6>Can't access my account?</h6></a></div>
-                                <div class="spanFull noMobileTablet yesMobilePhone"><h8>By logging in, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.</h8></div>
-                                </form>
-                                <!-- This is where the content ends -->
-                            </div>
-                            
-                      	</div>
-                  </div>
-                    
-                    <div class="bodyRow">
-                        <ul class="loginMenu noMobilePhone">
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Terms of Service</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">Contact Us</a></li>
-                        </ul>
-                        <div class="loginMenu">&copy; 2015 Stvdi</div>
-                        <div class="termsPolicy noMobilePhone yesMobileTablet"><h8>By logging in, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.</h8></div>
-                    </div>
-              </div>
-                    
-          </div>
-        </div>
-		<!-- End modalWrapper class -->
-
-	</body>
-</html>
