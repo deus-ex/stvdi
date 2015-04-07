1864e290aa02b22cbd2f84e77005a74666c2364e
JAY <j.ilukhor@gmail.com>
2015-04-07 13:47:59 +0100

    Updated page library
    I updated the page library to be able to check and load for custom pages
    before loading default pages and then normal pages

--- includes/libraries/page.class.php
+++ includes/libraries/page.class.php
@@ -89,7 +89,7 @@
     **/
     public function __constructor( $config = NULL ) {
       $this->errorPage = ( isset( $config['error_page'] ) ) ? $this->set_page( $config['error_page'] ) : $this->set_page( $this->errorPage );
-      $this->themeDirectory = ( isset( $config['theme_dir'] ) ) ? $config['theme_dir'] : $this->themeDirectory;
+      $this->themeDirectory = ( isset( $config['themes'] ) ) ? $config['themes'] : $this->themeDirectory;
     }
 
     /**
@@ -101,9 +101,27 @@
     *
     **/
     public function set_theme( $themeFolder ) {
-      $folder = $this->themeDirectory . str_replace( '/', '', $themeFolder ) . '/';
-      if ( TEMPLATE::is_found( $folder ) )
+      $themeFolder = ( substr( $themeFolder, -1 ) == '/' ) ? $themeFolder : $themeFolder . '/';
+      $folder = $this->themeDirectory . $themeFolder;
+      if ( $this->is_found( $folder ) )
         $this->defaultTheme = $themefolder;
+    }
+
+    /**
+    *
+    * Change template path
+    *
+    * @access public
+    * @param string $templateDir -> The template path
+    *
+    **/
+    public function set_template( $templateDir ) {
+      if ( empty( $templateDir ) || is_null( $templateDir ) ) {
+        $this->templatePath = str_replace( '\\', '/', dirname( __FILE__ ) );
+        $this->templatePath .= ( substr( $this->templateDir, -1 ) == '/' ) ? $this->templatePath : $this->templatePath . '/';
+      } else {
+        $this->templatePath = ( substr( $templateDir, -1 ) == '/' ) ? $templateDir : $templateDir . '/';
+      }
     }
 
     /**
@@ -125,29 +143,77 @@
 
     /**
     *
+    * Get page from either custom theme or default template
+    *
+    * @access private
+    * @return string
+    * @param string $file -> The page file
+    * @param string $folder -> The page file folder
+    *
+    **/
+    private function get_page( $file, $folder = NULL ) {
+      if ( ! empty( $folder ) ) {
+        $newFile = $folder . '/' . $file;
+        $indexFile = $folder . '/index.php';
+        $error = $folder . '/' . $this->errorPage;
+      }
+
+      $customPage = $this->themeDirectory . $this->defaultTheme . $newFile;
+      $defaultFileA = $this->templatePath . $file;
+      $defaultFileB = $this->templatePath . $newFile;
+      $defaultFileC = $this->templatePath . $indexFile;
+      $customError = $this->themeDirectory . $error;
+      $defaultError = $this->templatePath . $error;
+
+      if ( $this->is_found( $customPage ) ) {
+        return $customPage;
+      } else if ( $this->is_found( $defaultFileA ) ) {
+        return $defaultFileA;
+      } else if ( $this->is_found( $defaultFileB ) ) {
+        return $defaultFileB;
+      } else if ( $this->is_found( $defaultFileC ) ) {
+        return $defaultFileC;
+      } else {
+        return $this->get_error_page( $defaultError, $customError );
+      }
+    }
+
+    /**
+    *
+    * Get error page from custom theme or default template
+    *
+    * @access private
+    * @return string
+    * @param string $defaultError -> The default error file
+    * @param string $customError -> The custom error file
+    *
+    **/
+    private function get_error_page( $defaultError, $customError = NULL ) {
+        if ( $this->is_found( $customError ) ) {
+          return $customError;
+        } else if( $this->is_found( $defaultError ) ) {
+          return $defaultError;
+        } else {
+          return $this->errorPage;
+        }
+    }
+
+    /**
+    *
     * Validate page and prepare them for loading
     *
     * @access private
     * @return string
-    * @param string $page -> The page name to load
+    * @param string $pageStr -> The page name to load
     *
     **/
-    private function build( $pageStr ) {
-      $customPage = $this->themeDirectory . $this->defaultTheme . $this->set_page( $pageStr );
+    private function build( $pageStr = NULL, $folder = NULL ) {
       $file = $this->set_page( $pageStr );
-      $fileDir = $this->themeDirectory . $file;
-      $error = $this->themeDirectory . $this->errorPage;
-
       if ( $this->is_found( $file ) ) {
         return $file;
-      } else if ( $this->is_found( $customPage ) ) {
-        return $customPage;
-      } else if ( $this->is_found( $fileDir ) ) {
-        return $fileDir;
       } else {
-        return ( $this->is_found( $error ) ) ? $error : $this->errorPage;
+        return $this->get_page( $file, $folder );
       }
-
     }
 
     /**
@@ -159,8 +225,8 @@
     * @param string $page -> Page name
     *
     **/
-    private function set_page( $pageStr ) {
-      return $pageStr . $this->extension;
+    private function set_page( $pageStr = NULL ) {
+      return ( ! empty( $pageStr ) ) ? $pageStr . $this->extension : '';
     }
 
     /**
@@ -172,11 +238,11 @@
     * @param string $page -> The page path
     *
     **/
-    public function load( $pageStr = NULL ) {
+    public function load( $pageStr = NULL, $folder = NULL ) {
       extract( $GLOBALS );
 
-      if ( ! empty( $pageStr ) ) {
-        include( $this->build( $pageStr ) );
+      if ( ! empty( $pageStr ) || ! empty( $folder ) ) {
+        include( $this->build( $pageStr, $folder ) );
       } else {
         include( $this->defaultPage );
       }
